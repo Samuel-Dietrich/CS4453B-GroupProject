0619cbba6b04c1449ffbc511f9ddfef5649802ad
An Minh Tri <minhtri5813@gmail.com>
2026-03-25 21:24:51 -0400

    complete A2C training process

--- A2C/A2C_Train.py
+++ A2C/A2C_Train.py
@@ -1,12 +1,20 @@
+import torch as th
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
+from typing import Callable
+
+# Linear learning rate scheduler
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    def func(progress_remaining: float) -> float:
+        return progress_remaining * initial_value
+    return func
 
 print("Setup Environment...")
 
 # Create the environment
-env_id = "PongNoFrameskip"
-env = make_atari_env(env_id, n_envs=10, seed=0)
+env_id = "PongNoFrameskip-v4"
+env = make_atari_env(env_id, n_envs=16, seed=0)
 
 # Stack 4 frames
 env = VecFrameStack(env, n_stack=4)
@@ -17,18 +25,25 @@ print("Initialize A2C...")
 model = A2C(
     policy="CnnPolicy",
     env=env,
-    learning_rate=7e-4,
-    n_steps=128,
+    learning_rate=linear_schedule(7e-4),
+    n_steps=5, 
     ent_coef=0.01,
+    vf_coef=0.25,
+    gamma=0.99,
     verbose=1,
-    tensorboard_log="./../pong_tensorboard/Scratch"
+    tensorboard_log="./pong_tensorboard/Scratch",
+    policy_kwargs=dict(
+        optimizer_class=th.optim.RMSprop,
+        optimizer_kwargs=dict(alpha=0.99, eps=1e-5, weight_decay=0)
+    )
 )
 
-# 4. Train the agent
+# Train the agent
 print("Training started...")
-TOTALSTEPS = 500_000
-model.learn(total_timesteps=TOTALSTEPS, tb_log_name=f"A2C_{TOTALSTEPS/1_000_000}_Step")
+TOTALSTEPS = 10_000_000
+# log_interval indicates number of updates, now updates are much more frequent
+model.learn(total_timesteps=TOTALSTEPS, tb_log_name=f"A2C_{TOTALSTEPS/1_000_000}_Step", log_interval=100)
 
-# 5. Save the model
-model.save(f"a2c_pong_model_{TOTALSTEPS}")
+# Save the model
+model.save(f"./A2C/a2c_pong_model_{TOTALSTEPS}")
 print("Model saved!")
